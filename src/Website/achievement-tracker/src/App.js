6b66c5e24ebbed23a93d1a31866950e4c22870b3
Zanderthegreat12 <Zanderthegreat12@gmail.com>
2024-08-01 18:41:30 -0700

    Added the website to the Repo
    Added website to repo

---
+++ src/Website/achievement-tracker/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+
+
+  return (
+    <div>
+      <h1>Video Game Tracker</h1>
+
+    <script type="text/javascript" src="App.js"></script>
+  <h2> The Legend of Heroes: Trails into Reverie </h2>
+<img src = "https://image.api.playstation.com/trophy/np/NPWR33790_00_00464047B556A610EE5164528DDA81A368A9E34381/80A0768BF61793EF3431910CAA2C6A320601E8A9.PNG" ></img>
+<p> ---progress bar---- 41% </p>
+<p> trophies : *Platnum trophy image* 0/1  *Gold trophy image* 0/2  *silver trophy image* 0/8 *bronze trophy image* 29/42 </p>
+    </div>
+  );
+}
+
+export default App;
