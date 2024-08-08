0f67a749367ddd66acf2dad2a0ad16bba9591b76
Zanderthegreat12 <Zanderthegreat12@gmail.com>
2024-08-07 18:47:00 -0700

    Finished Setting up the Layout for Titles Page
    formatted the layout of the game titles page. I still need to implement proper CSS and modularize the code.

--- src/Website/achievement-tracker/src/App.js
+++ src/Website/achievement-tracker/src/App.js
@@ -20,12 +20,68 @@ function App() {
 function GamesList(){
 
   const gameList = games.map( (game) => 
-    <div>
-      <p>{game[0].trophyTitleName}</p>
-      <img src ={game[0].trophyTitleIconUrl}></img>
+    <div class = "GameOverview">
+      <div class = "imageSect">
+        <img src ={game[0].trophyTitleIconUrl}></img>
+      </div>
+      <div>
+        
+        <p>{game[0].trophyTitleName}</p>
+        
+        <div class = "trophySect">
+          <div class = "trophySubSect">
+          <img src = "https://wrapup.playstation.com/_ipx/w_3840,q_100//assets/trophies/antman-trophy-bronze.png?url=/assets/trophies/antman-trophy-bronze.png&w=3840&q=100" width="75%" height="75%" ></img>
+          </div>
+          <div class = "trophySubSect">
+          <p>{game[0].earnedTrophies.bronze}/{game[0].definedTrophies.bronze}</p>
+          </div>
+        </div>
+
+        <div class = "trophySect">
+          <div class = "trophySubSect">
+            <img src = "https://wrapup.playstation.com/_ipx/w_3840,q_100//assets/trophies/antman-trophy-silver.png?url=/assets/trophies/antman-trophy-silver.png&w=3840&q=100" width="75%" height="75%" ></img>
+          </div>
+          <div class = "trophySubSect">
+            <p> {game[0].earnedTrophies.silver}/{game[0].definedTrophies.silver}</p>
+          </div>
+        </div>
+        
+        <div class = "trophySect">
+          <div class = "trophySubSect">
+            <img src = "https://wrapup.playstation.com/_ipx/w_3840,q_100//assets/trophies/antman-trophy-gold.png?url=/assets/trophies/antman-trophy-bronze.png&w=3840&q=100" width="75%" height="75%" ></img>
+          </div>
+          <div class = "trophySubSect">
+            <p>{game[0].earnedTrophies.gold}/{game[0].definedTrophies.gold}</p>
+          </div>
+        </div>
+        
+        <div class = "trophySect">
+          <div class = "trophySubSect">
+            <img src = "https://wrapup.playstation.com/_ipx/w_3840,q_100//assets/trophies/antman-trophy-platinum.png?url=/assets/trophies/antman-trophy-bronze.png&w=3840&q=100" width="75%" height="75%" ></img>
+          </div>
+          <div class = "trophySubSect">
+            <p>{game[0].earnedTrophies.platinum}/{game[0].definedTrophies.platinum}</p>
+          </div>
+        </div>
+        
+        <div class = "ProgressBarCSS">
+          <ProgressBar progress={game[0].progress}/>
+        </div>
+      
+      </div>
     </div>)
 
   return  <ul>{gameList}</ul>;
+}
+
+function ProgressBar({progress}){
+  return(
+    <div class= "ParentProgress">
+      <div class = "ChildProgress" style = {{width: `${progress}%`}}>
+        <span>{`${progress}%`}</span> 
+      </div>
+    </div>
+  )
 }
 
 
