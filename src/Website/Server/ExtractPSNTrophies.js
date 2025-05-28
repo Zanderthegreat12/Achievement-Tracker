c290f3e19574e974fe4eb789e35f468dfa822cc0
Zanderthegreat12 <Zanderthegreat12@gmail.com>
2025-05-28 16:09:20 -0700

    Fixed gitignore

--- src/Website/Server/ExtractPSNTrophies.js
+++ src/Website/Server/ExtractPSNTrophies.js
@@ -1,7 +1,7 @@
 import * as PSNapi from "psn-api";
 import * as fs from 'fs';
 
-const Npsso = "";
+const Npsso = "aFptXMmPDoPKaTzPjHRxUrpYmfNo2bxKMEsOcaTdqpHakwBPE2qE1ws0EBEuvjBQ";
 
 async function ExtractPSNTrophies(name){
     try{
